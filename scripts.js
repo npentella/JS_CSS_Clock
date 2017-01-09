d20373ef25ebd536888e14499aaa0ee4c86f28f9
npentella <npentella@gmail.com>
2017-01-09 16:08:16 -0600

    Select clock hands, create setDate function to grab current time every second

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-console.log("linked!");
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+	var now = new Date();
+	console.log(now.getSeconds());
+}
+
+setInterval(setDate, 1000);
