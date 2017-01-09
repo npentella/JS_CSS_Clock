12b8f3903dfe46cf6a9f34b48ebaf86cd9dc3935
npentella <npentella@gmail.com>
2017-01-09 16:33:31 -0600

    Set minute hand when seconds is 0;

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const secondHand = document.querySelector(".second-hand");
-const minuteHand = document.querySelector(".minute-hand");
+const minuteHand = document.querySelector(".min-hand");
 const hourHand = document.querySelector(".hour-hand");
 
 function setSecondHand(seconds) {
@@ -8,12 +8,18 @@ function setSecondHand(seconds) {
 	return seconds;
 };
 
+function setMinuteHand(minutes) {
+	var position = ((minutes / 60) * 360) + 90;
+	console.log(minuteHand);
+	minuteHand.style.transform = `rotate(${ position }deg)`;
+}
+
 function setTime() {
 	var now = new Date();
 
 	setSecondHand(now.getSeconds());
 	if(!now.getSeconds()) {
-		
+		setMinuteHand(now.getMinutes());
 	};
 }
 
